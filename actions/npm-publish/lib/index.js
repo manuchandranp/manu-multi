cf50e3fd32fca58d5e8e05e5955cd2d2086f46f2
manuchandranp <122431000+manuchandranp@users.noreply.github.com>
2025-10-23 21:23:54 +0530

    Update index.js

--- actions/npm-publish/lib/index.js
+++ actions/npm-publish/lib/index.js
@@ -5,16 +5,9 @@ async function run() {
   try {
     const jfrogNpmRepository = core.getInput('jfrog-npm-repository');
     const jfrogUrl = core.getInput('jfrog-url');
-    const nodeVersion = core.getInput('node-version');
     const packageJsonPath = core.getInput('package-json-file-path');
     
-    // Set up Node.js version (if needed)
-    execSync(`npm install -g npm@latest`, { stdio: 'inherit' });
-    
-    // Show Node and NPM versions
-    console.log(`Node Version: ${process.version}`);
-    
-    // Set NPM registry and authentication
+    // Set up NPM registry and authentication
     execSync(`npm config set registry ${jfrogUrl}`);
     execSync(`npm config set //${jfrogUrl}:_authToken=${process.env.JFROG_ACCESS_TOKEN}`);
 
